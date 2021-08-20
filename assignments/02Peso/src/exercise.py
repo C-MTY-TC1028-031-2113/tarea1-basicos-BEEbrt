50d2a8913aef728da8b2c451431723d9774a185d
BEEbrt <A01746987@itesm.mx>
2021-08-20 13:09:24 -0500

    peso

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,8 +1,9 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
-
-
-
-if __name__ == '__main__':
-    main()
+def peso_mes():
+    inicial= 1
+    final= 0
+    mes= 12
+pinical =float(input("Dime tu peso inicial: "))
+pfinal =float(input("Dime tu peso final: "))
+mesesito =int(input("Dime en cuantos meses: "))
+resultado= (pinical - pfinal) / mesesito
+print ("Lo que se debe baja por mes es: ",resultado)
