1c768575af45299e9cebb8a3213b62b91b99066d
BEEbrt <A01746987@itesm.mx>
2021-08-20 10:49:19 -0500

    promedio

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -1,7 +1,10 @@
-def main():
-    #escribe tu código abajo de esta línea
-    print ("mensaje" )
-
-
-if __name__ == '__main__':
-    main()
+print("Programa para calcular promedios de valores")
+suma=0
+contador=0
+promedio=0
+valor_r=int(input("Introduzca el numero de materias "))
+for i in range(valor_r):
+    valor_n= int(input("Ingresa tus calificaciones: "))
+    suma=suma+valor_n
+promedio=suma/valor_r
+print("El promedio de las " + str(valor_r) +" materias es: " + str(promedio))
