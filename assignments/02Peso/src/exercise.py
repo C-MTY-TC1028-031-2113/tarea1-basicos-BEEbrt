bf8192451c637b5ea06e204c016d1aa92717e10d
BEEbrt <A01746987@itesm.mx>
2021-08-20 13:10:30 -0500

    peso

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,7 +1,3 @@
-def peso_mes():
-    inicial= 1
-    final= 0
-    mes= 12
 pinical =float(input("Dime tu peso inicial: "))
 pfinal =float(input("Dime tu peso final: "))
 mesesito =int(input("Dime en cuantos meses: "))
